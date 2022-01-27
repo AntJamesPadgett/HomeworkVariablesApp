7418c429595720215e5c8bd04a4d44c798abf283
antjamespadgett <padgettanthonyj@gmail.com>
2022-01-27 14:06:21 -0600

    did the work

--- HomeworkVariables/Program.cs
+++ HomeworkVariables/Program.cs
@@ -2,14 +2,19 @@
 
 string firstName;
 firstName = "Tony";
+Console.WriteLine(firstName);
 
 string lastName = "Padgett";
+Console.Writeline(lastName);
 
 int age = 56;
+Console.WriteLine(age);
 
 bool isAlive = true;
+Console.WriteLine(isAlive);
 
 string phoneNumber = "205-218-6393";
+Console.WriteLine(phoneNumber);
 
 // If yo are not calculating the numbers..use strings.
 
