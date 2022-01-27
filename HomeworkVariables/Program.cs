aefaa4773112d958a1d074b7e380be1cd76f7752
antjamespadgett <padgettanthonyj@gmail.com>
2022-01-27 13:44:46 -0600

    did the work

---
+++ HomeworkVariables/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+string firstName;
+firstName = "Tony";
+
+string lastName = "Padgett";
+
+int age = 56;
+
+bool isAlive = true;
+
+string phoneNumber = "205-218-6393";
+
+// If yo are not calculating the numbers..use strings.
+
+
